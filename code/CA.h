8374d5fd2e4584b1f38173a58d669d35bf2300fe
SwitchAxe <sofiacerasuoli@gmail.com>
2021-02-10 20:18:17 +0100

    trying to fix a segfault

--- code/CA.h
+++ code/CA.h
@@ -1,72 +1,75 @@
-#include <stdbool.h>
-#include <stdio.h>
-//defining the neighborhood
-#define C cellmap[x][y] //center of the neighborhood
-#define N cellmap[x][y-1] //North direction   
-#define NW cellmap[x-1][y-1] //North-West direction
-#define NE cellmap[x+1][y-1] //North-east direction 
-#define E  cellmap[x-1][y] //East direction
-#define W  cellmap[x+1][y] //West direction
-#define S  cellmap[x][y+1] //South direction 
-#define SW cellmap[x-1][y-1] //South-West direction
-#define SE cellmap[x+1][y+1] //South-East direction
+#ifndef CA_H
+	#include <stdbool.h>
+	#include <stdio.h>
+	//defining the neighborhood
+	#define C cellmap[x][y] //center of the neighborhood
+	#define N cellmap[x][y-1] //North direction   
+	#define NW cellmap[x-1][y-1] //North-West direction
+	#define NE cellmap[x+1][y-1] //North-east direction 
+	#define E  cellmap[x-1][y] //East direction
+	#define W  cellmap[x+1][y] //West direction
+	#define S  cellmap[x][y+1] //South direction 
+	#define SW cellmap[x-1][y+1] //South-West direction
+	#define SE cellmap[x+1][y+1] //South-East direction
 
-//defining the macro functions for easier and more readable loops
-#define cycleX(size) for (int x = 0; x < (size); x++)
-#define cycleY(size) for (int y = 0; y < (size); y++)
+	//defining the macro functions for easier and more readable loops
+	#define cycleX(size) for (int x = 0; x < (size); x++)
+	#define cycleY(size) for (int y = 0; y < (size); y++)
 
-//oS is the old state of the cell, nS is the new state.
-typedef struct
-{
-	bool oS;
-	bool nS;
-} cell;
 
-void clearScreen()
-{
-	printf("\033[0;0H\033[0;0f");
-}
-
-void printCellmap(int size, cell cellmap[size][size])
-{
-	
-	for (int x = 0; x < size; x++)
+	//oS is the old state of the cell, nS is the new state.
+	typedef struct
 	{
-		for (int y = 0; y < size; y++)
-		{
-			printf("%c", (C.oS == true) ? 'x' : ' ');
-		}
-		printf("\n");
+		bool oS;
+		bool nS;
+	} cell;
+
+	void clearScreen()
+	{
+		printf("\033[0;0H\033[0;0f");
 	}
-}
-void updateCellmap(int size, cell cellmap[size][size])
-{
-	for (int x = 0; x < size; x++)
+
+	void printCellmap(int size, cell cellmap[size][size])
 	{
-		for (int y = 0; y < size; y++)
+		
+		for (int x = 0; x < size; x++)
 		{
-			C.oS = C.nS;
+			for (int y = 0; y < size; y++)
+			{
+				printf("%c", (C.oS == true) ? 'x' : ' ');
+			}
+			printf("\n");
 		}
 	}
-}
-
-void fillCharMap(int size, char charmap[size][size], FILE* file)
-{
-	for (int i = 0; i < size; i++)
+	void updateCellmap(int size, cell (*cellmap)[size][size])
 	{
-		fscanf(file, "%s", charmap[i]);
-	}
-}
-
-void CellmapFromCharmap(int size, 
-						cell cellmap[size][size],
-						 char charmap[size][size])
-{
-	cycleX(size)
-	{
-		cycleY(size)
+		for (int x = 0; x < size; x++)
 		{
-			C.oS = (charmap[x][y] == '1') ? true : false;
+			for (int y = 0; y < size; y++)
+			{
+				C->oS = C->nS;
+			}
 		}
 	}
-}
+
+	void fillCharmap(int size, char (*charmap)[size][size], FILE* file)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			fscanf(file, "%s", *charmap[i]);
+		}
+	}
+
+	void CellmapFromCharmap(int size, 
+							cell (*cellmap)[size][size],
+							char (*charmap)[size][size])
+	{
+		cycleX(size)
+		{
+			cycleY(size)
+			{
+				C->oS = (*charmap[x][y] == '1') ? true : false;
+			}
+		}
+	}
+#endif
--- code/example.c
+++ code/example.c
@@ -3,25 +3,53 @@
 #include <unistd.h>
 #define SIZE 24
 
+int LST(int size, cell cellmap[size][size], int x, int y)
+{
+    //Local State Function 	
+	int adjcnt = 0;
+	if ((x-1 >= 0) && (y-1 >= 0) && NW.oS) adjcnt++;
+	if ((y+1 < SIZE) && (x-1 >= 0) && SW.oS) adjcnt++;
+	return adjcnt;
+}
+
+void updateStates(int size, cell (*cellmap)[size][size])
+{
+    cycleX(size)
+    {
+        cycleY(size)
+        {
+            if (C->oS) {continue;}
+            if (LST(SIZE, *cellmap, x, y) == 1)
+            {
+                C->nS = true;
+            }
+            else
+            {
+                C->nS = false;
+            }
+        }
+    }
+}
 
 int main()
 {
-    cell cellmap[SIZE][SIZE];
+    cell cellmap[SIZE][SIZE] = {0};
     
-    char** charmap = malloc(SIZE*sizeof(char*));
-    for (int i = 0; i < SIZE;i++)
-    {
-        charmap[i] = malloc(SIZE*sizeof(char));
-    }
+    char charmap[SIZE][SIZE] = {0};
     FILE* file = fopen("example_input", "r");
-    fillCharMap(SIZE, charmap, file);
-    CellmapFromCharmap(SIZE, cellmap, charmap);
+    
+    if (file == NULL) {printf("error opening the file"); exit(1);}
+    fillCharmap(SIZE, &charmap, file);
+    printf("got here!\n");
+    CellmapFromCharmap(SIZE, &cellmap, &charmap);
     while (true)
     {
-        updateCellmap(SIZE, cellmap);
-        printCellmap(SIZE, cellmap);
         clearScreen();
-        usleep(1);
+        
+        updateCellmap(SIZE, &cellmap);
+        updateStates(SIZE, &cellmap);
+        printCellmap(SIZE, cellmap);
+        usleep(100000);
     }
 
     return 0;
